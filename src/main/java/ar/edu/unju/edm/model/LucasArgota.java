aa900e515a45f5dd0b716fc03cf196f4de80e803
Lucas <Lucas@DESKTOP-FVGHMLF>
2021-05-13 15:42:03 -0300

    Lucas Argota
    Agregado de getters and setters

--- src/main/java/ar/edu/unju/edm/model/LucasArgota.java
+++ src/main/java/ar/edu/unju/edm/model/LucasArgota.java
@@ -1,5 +1,31 @@
 package ar.edu.unju.edm.model;
 
+import org.springframework.stereotype.Component;
+
+@Component 
 public class LucasArgota {
+	private Integer idalumno;
+	private String nombre ;
+	private String apellido ;
+	public Integer getIdalumno() {
+		return idalumno;
+	}
+	public void setIdalumno(Integer idalumno) {
+		this.idalumno = idalumno;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	} 
+	
+	
 
 }
