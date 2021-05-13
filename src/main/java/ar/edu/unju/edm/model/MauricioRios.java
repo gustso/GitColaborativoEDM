9dfe2f918415257c7ca9058b0c57d029c401414c
Mauri <Mauri@192.168.0.14>
2021-05-13 15:18:16 -0300

    Agregado de atributos a mi clase

--- src/main/java/ar/edu/unju/edm/model/MauricioRios.java
+++ src/main/java/ar/edu/unju/edm/model/MauricioRios.java
@@ -1,5 +1,12 @@
 package ar.edu.unju.edm.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MauricioRios {
+	
+	private Integer idClase; 
+	private String nombre; 
+	private String apellido;
 
 }
